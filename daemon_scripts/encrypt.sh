d6eef6f61a0bc10fe901dfd0b665440f00fc28fa
pbca26 <pbca26@gmail.com>
2016-11-29 12:06:38 -0500

    added encrypt wallet bash script

--- daemon_scripts/encrypt.sh
+++ daemon_scripts/encrypt.sh
@@ -12,6 +12,11 @@ echo "$syscoin removed"
 #echo "$mazacoin removed"
 
 ./bin/syscoind -regtest -daemon
+sleep 1
+./bin/syscoin-cli -regtest encryptwallet "test test"
+sleep 5
+./bin/syscoind -regtest -daemon
+sleep 1
 
-cd ../
-npm test
+#cd ../
+#npm test
