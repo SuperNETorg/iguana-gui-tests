fad9ba767f34a558df9f6260f27f4873ada99d78
pbca26 <pbca26@gmail.com>
2016-12-02 07:32:28 -0500

    added gen tx; tx unit test

---
+++ test/e2e/single-session/dashboard-check-nonempty-sys-wallet.js
@@ -0,0 +1,51 @@
+var conf = require('../../../nightwatch.conf.js'),
+    iguanaGUIFolder = 'file:///home/pbca/Iguana-GUI/compiled/dev/',
+    step = -1,
+    chalk = require('chalk'),
+    util = require('util'),
+    fs = require('fs'),
+    generatedPassphraseText,
+    exec = require('child_process').exec,
+    child,
+    currency = 'usd',
+    coin = 'sys',
+    genSysVal = 474200578,
+    transactionAmount = 54.13,
+    coinFullName = 'Syscoin',
+    txData,
+    defaultAddress;
+
+function getScreenshotUrl() {
+  step++;
+  return 'screenshots/dashboard-check-nonempty-sys-wallet' + step +'.png';
+}
+
+function getTx() {
+  return JSON.parse(fs.readFileSync('listtransactions.txt', 'utf-8'))[0];
+}
+
+module.exports = {
+  'test IguanaGUI check dashboard w/ empty sys wallet': function(browser) {
+    browser
+      .verify.containsText('.balance-block .currency', currency.toUpperCase())
+    browser // check sidebar values
+      .verify.cssClassPresent('.account-coins-repeater .sys', 'active')
+      .verify.containsText('.account-coins-repeater .sys .name', coinFullName)
+      .verify.containsText('.account-coins-repeater .sys .coin-value', coin.toUpperCase())
+      .verify.containsText('.account-coins-repeater .sys .coin-value .val', genSysVal)
+      .verify.containsText('.account-coins-repeater .sys .currency-value', currency.toUpperCase())
+    browser // check transaction unit balances
+      .verify.cssClassPresent('.coins .btn-add-coin', 'disabled')
+      .verify.cssClassNotPresent('.transactions-unit .action-buttons .btn-send', 'disabled')
+      .waitForElementNotPresent('.transactions-unit .top-bar .loader')
+      .waitForElementNotPresent('.transactions-unit .transactions-list .loader')
+    browser // the first tx should be of receive category and in process
+      .waitForElementVisible('.transactions-list-repeater .item:first-child')
+      .verify.cssClassPresent('.transactions-list-repeater .item:first-child', 'process')
+      .verify.cssClassPresent('.transactions-list-repeater .item:first-child div:nth-child(3)', 'progress-status')
+      .verify.containsText('.transactions-list-repeater .item:first-child .status', 'In Process')
+      .verify.containsText('.transactions-list-repeater .item:first-child .amount .value', getTx().amount)
+      .verify.containsText('.transactions-list-repeater .item:first-child .amount .coin-name', coin.toUpperCase())
+      .verify.containsText('.transactions-list-repeater .item:first-child .hash', getTx().address)
+  }
+};
---
+++ test/e2e/single-session/syscoin-gen-coins.js
@@ -0,0 +1,55 @@
+var conf = require('../../../nightwatch.conf.js'),
+    iguanaGUIFolder = 'file:///home/pbca/Iguana-GUI/compiled/dev/',
+    step = -1,
+    generatedPassphraseText,
+    chalk = require('chalk'),
+    util = require('util'),
+    fs = require('fs'),
+    exec = require('child_process').exec,
+    child;
+
+function getScreenshotUrl() {
+  step++;
+  return 'screenshots/signup-after-wallet-encrypt-' + step +'.png';
+}
+
+module.exports = {
+  'test IguanaGUI execute syscoin regtest generate': function(browser) {
+    browser
+      .pause(100, function() {
+        console.log('gen sys coins')
+        child = exec('./daemon_scripts/bin/syscoin-cli -regtest generate 101', function(error, stdout, stderr) {
+          console.log('stdout: ' + stdout)
+          console.log('stderr: ' + stderr)
+          if (error !== null) {
+            console.log('exec error: ' + error)
+          }
+        })
+      })
+      .pause(1000, function() {
+        console.log('get last transaction')
+        child = exec('./daemon_scripts/bin/syscoin-cli -regtest listtransactions "" 1', function(error, stdout, stderr) {
+          console.log('stdout: ' + stdout)
+          console.log('stderr: ' + stderr)
+          if (error !== null) {
+            console.log('exec error: ' + error)
+          }
+          fs.writeFileSync('listtransactions.txt', stdout, 'utf-8')
+        })
+      })
+      .pause(1000, function() {
+        console.log('get account address')
+        child = exec('./daemon_scripts/bin/syscoin-cli -regtest getaccountaddress ""', function(error, stdout, stderr) {
+          console.log('stdout: ' + stdout)
+          console.log('stderr: ' + stderr)
+          if (error !== null) {
+            console.log('exec error: ' + error)
+          }
+          fs.writeFileSync('accountaddress.txt', stdout, 'utf-8')
+        })
+      })
+      .pause(1000)
+      .url(iguanaGUIFolder + 'index.html#/dashboard')
+      .pause(2000)
+  }
+};
