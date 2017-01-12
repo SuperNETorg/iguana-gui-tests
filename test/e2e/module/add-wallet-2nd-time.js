8f9ae282869c78b9631ea8865b352d290559db63
pbca26 <pbca26@gmail.com>
2017-01-12 20:43:36 +0300

    updated add wallet (coind) test

--- test/e2e/module/add-wallet-2nd-time.js
+++ test/e2e/module/add-wallet-2nd-time.js
@@ -1,42 +1,8 @@
-var conf = require('../../../nightwatch.conf.js');
+var conf = require('../../../nightwatch.conf.js'),
+    testName = 'login-add-wallet-sys-2nd-time';
 
 module.exports = {
   'test IguanaGUI execute add a wallet 2nd time': function(browser) {
-    var getScreenshotUrl = (function(name) {
-        var counter = -1;
-
-        return function () {
-          counter += 1;
-          return 'screenshots/' + browser.globals.test_settings.mode + '/' + name + '-' + counter + '-{{ res }}-' + '.png';
-        }
-    })('login-add-wallet-sys-2nd-time');
-
-    var responsiveTest = function(containerToScroll) {
-      for (var a=0; a < browser.globals.test_settings.scrollByPoinsCount; a++) {
-        for (var i=0; i < browser.globals.test_settings.responsiveBreakPoints.length; i++) {
-          var viewport = browser.globals.test_settings.responsiveBreakPoints[i].split(' x ')
-          browser
-            .resizeWindow(Number(viewport[0]) + 10, Number(viewport[1]) + 80)
-            .execute(function(container, run) {
-              if (container) {
-                var elem = document.querySelector(container);
-                if (run === 0) {
-                  if (container === 'window')
-                    window.scrollBy(0, document.querySelector('html').offsetHeight * -1);
-                  else
-                    elem.scrollTop = 0;
-                } else {
-                  if (container === 'window')
-                    window.scrollBy(0, document.querySelector('html').offsetHeight / run);
-                  else
-                    elem.scrollTop = Math.floor(document.querySelector(container).offsetHeight / run);
-                }
-              }
-            }, [containerToScroll, a])
-            .saveScreenshot(getScreenshotUrl().replace('{{ res }}', '-scroll-' + a + '-' + browser.globals.test_settings.responsiveBreakPoints[i].replace(' x ', 'x')))
-        }
-      }
-    }
 
     browser
       .pause(3000)
@@ -45,15 +11,15 @@ module.exports = {
         browser
           .mouseButtonClick('left')
           .pause(250)
-          .waitForElementVisible('.flow-modal-login-state')
+          /*.waitForElementVisible('.flow-modal-login-state')
           .pause(10, function() {
-            responsiveTest('.flow-modal .modal-content')
+            conf.responsiveTest('.flow-modal .modal-content', testName, browser)
           })
-          .click('.after-flow-btn')
+          .click('.after-flow-btn')*/
           .waitForElementVisible('.add-new-coin-form-login-state')
           .pause(1000)
           .pause(10, function() {
-            responsiveTest('.auth-add-coin-modal .modal-content')
+            conf.responsiveTest('.auth-add-coin-modal .modal-content', testName, browser)
           })
           .setValue('.quick-search input[type=text]', ['someunknowncoin'])
           .waitForElementNotPresent('.supported-coins-repeater-inner .coin', 500)
@@ -73,7 +39,7 @@ module.exports = {
           .click('.supported-coins-repeater-inner .coin.sys')
           .waitForElementNotPresent('.add-new-coin-form-login-state', 500)
           .pause(10, function() {
-            responsiveTest('window')
+            conf.responsiveTest('window', testName, browser)
           })
       })
   }
