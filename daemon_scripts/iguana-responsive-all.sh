a559a702e3d79d3085c384c65fcd9f75feb63724
pbca26 <pbca26@gmail.com>
2017-01-22 17:59:22 +0300

    commented out screenhots rm bit in bash scripts

--- daemon_scripts/iguana-responsive-all.sh
+++ daemon_scripts/iguana-responsive-all.sh
@@ -17,15 +17,15 @@ pkill java
 npm run clean-iguana iguana_responsive iguana-signin-add-2nd-wallet.js skip
 pkill Xvfb
 
-for i in `seq 1 4`;
-do
-  md5sum `find ./screenshots -type f` | sort -k1 | uniq -w32 -d | xargs rm -fv
-done
-
-for i in `seq 1 4`;
-do
-  md5sum `find ./screenshots -type f` | sort -k1 | uniq -w32 -d | xargs rm -fv
-done
+#for i in `seq 1 4`;
+#do
+#  md5sum `find ./screenshots -type f` | sort -k1 | uniq -w32 -d | xargs rm -fv
+#done
+#
+#for i in `seq 1 4`;
+#do
+#  md5sum `find ./screenshots -type f` | sort -k1 | uniq -w32 -d | xargs rm -fv
+#done
 
 find ./screenshots -type f | xargs -n 1 bash -c 'convert "$0" "${0%.*}.jpg"'
 find ./screenshots -type f -name "*.png" | xargs rm -fv
